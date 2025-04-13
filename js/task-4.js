61cee4a18887b5c48f4ea4f00d50d885048cc0ab
vito <vito333@ukr.net>
2025-04-13 17:56:01 +0300

    1234

--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,21 @@
 function getShippingCost(country) {
-    
+    let price;
+    switch (country) {
+        case "Australia":
+            price = 170;
+            break;
+        case "Chile":
+            price = 250;
+            break;
+        case "China":
+            price = 100;
+            break;
+        case "Jamaica":
+            price = 120;
+            break;
+        default:
+            return "Sorry, there is no delivery to your country";
+    }
+    return `Shipping to ${country} will cost ${price} credits`;
 }
+// console.log(getShippingCost("Jamaica"));
